ade1b896d95d572049a6db5c7d3300279b461d96
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-11-08 18:15:05 +0100

    Update extern/solver subproject commit hash and enhance failure handling in executor_theory

--- src/executor_theory.cpp
+++ src/executor_theory.cpp
@@ -9,8 +9,10 @@ namespace ratio::executor
 
     void executor_theory::failure(const std::unordered_set<const ratio::atom *> &atoms)
     {
+        std::vector<utils::lit> cnfl;
         for (const auto &atm : atoms)
             cnfl.push_back(!atm->get_sigma());
+        set_theory_conflict(std::move(cnfl));
         // we backtrack to a level at which we can analyze the conflict..
         if (!backtrack_analyze_and_backjump() || !exec.get_solver().solve())
             throw riddle::unsolvable_exception();
